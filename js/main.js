74cd2d24e2543fbb37c8d39f67e9eb5301b2a370
biktor12487@gmail.com <biktor12487@gmail.com>
2017-08-26 23:02:25 +0300

    res main

--- js/main.js
+++ js/main.js
@@ -8,15 +8,27 @@ $(document).ready(function(){
   	autoWidth: true,
   });
 });
-$(document).ready(function(){
-  $("#popularProducts-slider").owlCarousel({
-  	items: 4,
-  	dots: false,
-  	nav: true,
-  	navText: false,
-  	margin: 0,
-  });
-});
+// $(document).ready(function(){
+//   $("#popularProducts-slider").owlCarousel({
+//   	items: 4,
+//   	dots: false,
+//   	nav: true,
+//   	navText: false,
+//   	margin: 0,
+//     responsive:{ 
+//       1024:{
+//         items: 4,
+//       },
+//       768:{
+//         items:3
+//       }
+//       ,
+//       0:{
+//         items: 1,
+//       }
+//     }
+//   });
+// });
 $(document).ready(function(){
   $("#long-slider").owlCarousel({
   	items: 1,
@@ -44,17 +56,66 @@ $(document).ready(function(){
     margin: 10,
   });
 });
-var navCategory = function(){
-	var el = document.querySelectorAll('.navbar > ul > li');
-	for (var i = el.length - 1; i >= 0; i--) {
-		el[i].onmouseover = function(){
-			this.setAttribute('data-status','visible')
-		}
-		el[i].onmouseleave = function(){
-			this.setAttribute('data-status','hidden')
-		}
-	}
+function absolutePx(str){
+  return Number(str.substr(0,str.length -2));
+}
+function nave(){
+      var widDisp = document.documentElement.clientWidth;
+      if (widDisp < 768) {
+        var navCategory = function(){
+        var el = document.querySelectorAll('.categoryNav .navbar > ul > li');
+      for (var i = el.length - 1; i >= 0; i--) {
+        el[i].setAttribute('data-height',absolutePx(getComputedStyle(el[i].querySelector('.navContent')).height));
+        el[i].setAttribute('data-status', 'hidden');
+        el[i].querySelector('.navContent').style.cssText = 'height: 0px';
+      }
+      var el = document.querySelectorAll('.navbar > ul > li');
+      for (var i = el.length - 1; i >= 0; i--) {
+        el[i].onclick = function(){
+
+          if (document.querySelectorAll('.navbar > ul > li[data-status="visible"]').length) {
+            document.querySelector('.navbar > ul > li[data-status="visible"]').querySelector('.navContent').style.height = 0+'px';
+            document.querySelector('.navbar > ul > li[data-status="visible"]').setAttribute('data-status','hidden');
+          }
+          this.setAttribute('data-status','visible');
+          var needHeight = this.getAttribute('data-height');
+          $(this.querySelector('.navContent')).animate({height: needHeight+'px'},100);
+        }
+      }
+      document.querySelector('#popularProducts-slider').setAttribute('class1','.owl-carousel-disable')
+    }();
+    var el = document.querySelector('.categoryNav .navbar .burger');
+    el.onclick = function(e){
+        var status = this.parentNode.parentNode.getAttribute('data-pon');
+        if (status == 'hidden') {
+          this.parentNode.parentNode.setAttribute('data-pon','visab');
+        }
+        else{
+          this.parentNode.parentNode.setAttribute('data-pon','hidden');
+        }
+    }
+  }
+  else{
+     var el = document.querySelectorAll('.categoryNav .navbar > ul > li');
+      for (var i = el.length - 1; i >= 0; i--) {
+        el[i].setAttribute('data-status', 'hidden');
+        el[i].querySelector('.navContent').style.height = 'auto';
+      }
+    var navCategory = function(){
+  var el = document.querySelectorAll('.navbar > ul > li');
+  for (var i = el.length - 1; i >= 0; i--) {
+    el[i].onmouseover = function(){
+      this.setAttribute('data-status','visible')
+    }
+    el[i].onmouseleave = function(){
+      this.setAttribute('data-status','hidden')
+    }
+  }
 }();
+  }
+}
+window.addEventListener('load',nave) 
+window.addEventListener('resize',nave) 
 var chekbox = function(){
 	var el = document.querySelectorAll('.psevodCheckBox');
 	for (var i = el.length - 1; i >= 0; i--) {
@@ -108,8 +169,10 @@ var countField = function(){
 }();
 var sliderMint = function(){
   var index = 0;
-  var slider = document.querySelector('.sliderMint');
-  var el = document.querySelectorAll('.link');
+  var slider = document.querySelectorAll('.sliderMint');
+  if (slider.length) {
+    slider = slider[0];
+      var el = document.querySelectorAll('.link');
   for (var i = el.length - 1; i >= 0; i--) {
     el[i].setAttribute('data-index',i)
   }
@@ -158,7 +221,7 @@ var sliderMint = function(){
   }
     
   }
-
+  }
 }();
 var tabsJust = function(){
   var el = document.querySelectorAll('.tabs');
@@ -203,4 +266,34 @@ var tabses = function(){
       }
      }
   }       
+}();
+var carouselMe = function(){
+  var Index = 0;
+  var count = 4;
+   var widDisp = document.documentElement.clientWidth;
+   if (widDisp < 1024 && widDisp > 768) {
+    count = 3;
+      var num = document.querySelectorAll('.obl-carousel .card').length -count;
+   }
+  else if (widDisp < 767){
+    count = 1;
+      var num = document.querySelectorAll('.obl-carousel .card').length/4 -1;
+   }
+   else{
+      var num = document.querySelectorAll('.obl-carousel .card').length -count;
+   }
+  document.querySelector('.obl-carousel .owl-next').onclick = function(){
+     if (Index < num) {
+      Index++;
+      var widthSlide = $('.obl-carousel .card').outerWidth()+absolutePx(getComputedStyle(document.querySelector('.obl-carousel .card')).marginLeft)+absolutePx(getComputedStyle(document.querySelector('.obl-carousel .card')).marginRight);
+      $('.obl-carousel .holste').animate({marginLeft: absolutePx(getComputedStyle(document.querySelector('.obl-carousel .holste')).marginLeft)-widthSlide+'px'},300);
+      }
+  }
+  document.querySelector('.obl-carousel .owl-prev').onclick = function(){
+    if (Index > 0) {
+      Index--;
+      var widthSlide = $('.obl-carousel .card').outerWidth()+absolutePx(getComputedStyle(document.querySelector('.obl-carousel .card')).marginLeft)+absolutePx(getComputedStyle(document.querySelector('.obl-carousel .card')).marginRight);
+    $('.obl-carousel .holste').animate({marginLeft: absolutePx(getComputedStyle(document.querySelector('.obl-carousel .holste')).marginLeft)+widthSlide+'px'},300);
+    }
+  }
 }();
